fe43f97c6b72c703e46fb720753f1e478c7d9070
younixue3 <bussiness.needs.rctka@gmail.com>
2025-08-01 16:33:26 +0800

    feat(homepage): update route and image assets
    - Change homepage route from '/home_page' to '/' for better UX
    - Replace project section images with new assets
    - Remove certification tab from legal section as it's no longer needed

--- controller/home_controller.py
+++ controller/home_controller.py
@@ -3,7 +3,7 @@ from odoo.http import request
 
 class HomePageController(http.Controller):
 
-    @http.route('/home_page', type='http', auth='public', website=True)
+    @http.route('/', type='http', auth='public', website=True)
     def home_page(self, **kwargs):
         # Render the form page template
         return request.render('mpa_website.home_page')
